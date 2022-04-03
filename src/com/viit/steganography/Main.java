1ccf16b487a0d2f0e8f765ae7251a36e858581a9
Bhavesh Patil <bhavesh.22020176@viit.ac.in>
2022-04-03 23:55:11 +0530

    First Commit

---
+++ src/com/viit/steganography/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.viit.steganography;
+
+import com.viit.steganography.ui.MainWindow;
+
+/**
+ * @author mtalaat
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		new MainWindow().show();
+
+	}
+
+}
